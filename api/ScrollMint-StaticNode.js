b1885d23cba70262dc0a4cab0f6e1f1f40d8e939
Yahawashiservant <keithdwhitfield@gmail.com>
2025-04-02 20:02:50 -0400

    Update ScrollMint-StaticNode.js

--- api/ScrollMint-StaticNode.js
+++ api/ScrollMint-StaticNode.js
@@ -1,3 +1,5 @@
+// Trigger scrollmint redeploy
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
